76b7e7f8a2f4e6ed6fa9a2097eef2c3e71dac48a
aumhaa <aumhaa@gmail.com>
2018-09-04 23:15:19 -0700

    Update to AumPush_OJI (live 10.0.3 compatibility) and drumchooser (added favorites, sample navigation gui buttons, fixed pad swap issues).

--- AumPush_OJI/AumPush.py
+++ AumPush_OJI/AumPush.py
@@ -21,10 +21,11 @@ from ableton.v2.control_surface.components import MixerComponent, TransportCompo
 from ableton.v2.control_surface.skin import *
 from ableton.v2.base import inject, clamp, nop, const, NamedTuple, listens, listens_group, find_if, mixin, forward_property, first, NamedTuple, in_range, flatten, task
 from ableton.v2.control_surface import BackgroundLayer, ClipCreator, ControlSurface, DeviceBankRegistry, Layer, midi
-from ableton.v2.control_surface.components import ViewControlComponent, BackgroundComponent, M4LInterfaceComponent, ModifierBackgroundComponent, SessionNavigationComponent, SessionRingComponent, SessionOverviewComponent, ViewControlComponent
+from ableton.v2.control_surface.components import ViewControlComponent, BackgroundComponent, ModifierBackgroundComponent, SessionNavigationComponent, SessionRingComponent, SessionOverviewComponent, ViewControlComponent
 from ableton.v2.control_surface.elements import adjust_string, ButtonElement, ButtonMatrixElement, ChoosingElement, ComboElement, DoublePressContext, MultiElement, OptionalElement, to_midi_value
-from ableton.v2.control_surface.mode import CompoundMode, AddLayerMode, LazyComponentMode, ReenterBehaviour, ModesComponent, EnablingModesComponent
+from ableton.v2.control_surface.mode import CompoundMode, AddLayerMode, ModesComponent
 from ableton.v2.control_surface.input_control_element import ParameterSlot
+from ableton.v2.control_surface.elements import ButtonElement
 
 from Push.push import Push
 from Push.device_navigation_component import DeviceNavigationComponent
@@ -36,7 +37,7 @@ from Push.mode_behaviours import CancellableBehaviour
 from Push import sysex
 
 from pushbase.value_component import ValueComponent, ParameterValueComponent, ValueDisplayComponent, ParameterValueDisplayComponent
-from pushbase.configurable_button_element import ConfigurableButtonElement
+#from pushbase.configurable_button_element import ConfigurableButtonElement
 from pushbase.instrument_component import InstrumentComponent
 from pushbase.step_seq_component import StepSeqComponent
 from pushbase.loop_selector_component import LoopSelectorComponent
@@ -49,11 +50,11 @@ from pushbase.touch_encoder_element import TouchEncoderElement
 from pushbase.touch_strip_element import TouchStripElement
 from pushbase.touch_strip_controller import TouchStripControllerComponent, TouchStripEncoderConnection
 from pushbase.selection import PushSelection
-from pushbase.accent_component import AccentComponent
-from pushbase.auto_arm_component import AutoArmComponent
+#from pushbase.accent_component import AccentComponent
+#from pushbase.auto_arm_component import AutoArmComponent
 from pushbase.matrix_maps import *
 from pushbase.consts import *
-from pushbase.scrollable_list_component import ScrollableListWithTogglesComponent
+#from pushbase.scrollable_list_component import ScrollableListWithTogglesComponent
 from pushbase.device_parameter_component import DeviceParameterComponent
 from pushbase.skin_default import make_default_skin
 
@@ -67,6 +68,7 @@ from aumhaa.v2.control_surface.mod_devices import *
 from aumhaa.v2.control_surface.components.mono_instrument import *
 from aumhaa.v2.base.debug import *
 from aumhaa.v2.control_surface.mod import *
+from aumhaa.v2.control_surface.components.m4l_interface import M4LInterfaceComponent
 
 from .Map import *
 
@@ -93,17 +95,17 @@ class AumPush(Push):
 			self._device_component._alt_pressed = False
 			self.set_feedback_channels(FEEDBACK_CHANNELS)
 		self.log_message('<<<<<<<<<<<<<<<<<<<<<<<< AumPush ' + str(self._monomod_version) + ' log opened >>>>>>>>>>>>>>>>>>>>>>>>')
-	
+
 
 	def _create_skin(self):
 		debug('creating skin colors')
 		return merge_skins(make_default_skin(), Skin(AumPushColors))
-	
+
 
 	@listens('failure')
 	def _on_handshake_failure(self, bootloader_mode):
 		debug('Handshake failed.')
-	
+
 
 	@listens('success')
 	def _on_handshake_success(self):
@@ -119,11 +121,11 @@ class AumPush(Push):
 				del settings['aftertouch_threshold']
 				self._user.settings = settings
 		self._device_provider.reevaluate_device()
-	
+
 
 	def _init_value_components(self):
 		debug('init value components')
-	
+
 
 	def _setup_mod(self):
 
@@ -132,9 +134,9 @@ class AumPush(Push):
 		with inject(register_component = const(self._register_component), song = const(self.song)).everywhere():
 			self.modhandler = PushModHandler(self) ## song = self.song, register_component = self._register_component)
 		self.modhandler.name = 'ModHandler'
-		self.modhandler.layer = Layer( priority = 10, lock_button = self.elements.note_mode_button, 
+		self.modhandler.layer = Layer( priority = 10, lock_button = self.elements.note_mode_button,
 																			grid = self.elements.matrix,
-																			key_buttons = self.elements.select_buttons, 
+																			key_buttons = self.elements.select_buttons,
 																			key2_buttons = self.elements.track_state_buttons,
 																			Shift_button = self.elements.shift_button,
 																			Alt_button = self.elements.select_button,
@@ -142,39 +144,39 @@ class AumPush(Push):
 																			master_dial = self.elements.master_volume_control,
 																			name_display_line = self.elements.display_line3,
 																			value_display_line = self.elements.display_line4 )
-		self.modhandler.legacy_shift_layer = AddLayerMode( self.modhandler, Layer(priority = 6, 
-																			nav_up_button = self.elements.nav_up_button, 
-																			nav_down_button = self.elements.nav_down_button, 
-																			nav_left_button = self.elements.nav_left_button, 
+		self.modhandler.legacy_shift_layer = AddLayerMode( self.modhandler, Layer(priority = 6,
+																			nav_up_button = self.elements.nav_up_button,
+																			nav_down_button = self.elements.nav_down_button,
+																			nav_left_button = self.elements.nav_left_button,
 																			nav_right_button = self.elements.nav_right_button,
-																			channel_buttons = self.elements.matrix.submatrix[:, 1:2], 
+																			channel_buttons = self.elements.matrix.submatrix[:, 1:2],
 																			nav_matrix = self.elements.matrix.submatrix[4:8, 2:6] ))
 		self.modhandler.shift_layer = AddLayerMode( self.modhandler, Layer( priority = 6,
 																			lock_button = self.elements.master_select_button, ))
 																			#device_selector_matrix = self.elements.matrix.submatrix[:, :1],
-		self.modhandler.alt_layer = AddLayerMode( self.modhandler, Layer( priority = 6, 
-																			alt_name_display_line = self.elements.display_line3, 
-																			alt_value_display_line = self.elements.display_line4 )) 
-	
+		self.modhandler.alt_layer = AddLayerMode( self.modhandler, Layer( priority = 6,
+																			alt_name_display_line = self.elements.display_line3,
+																			alt_value_display_line = self.elements.display_line4 ))
+
 
 	def _init_matrix_modes(self):
 		super(AumPush, self)._init_matrix_modes()
 		self._setup_mod()
 		self._note_modes.add_mode('mod', [self.modhandler])
 		self._note_modes.add_mode('looperhack', [self._audio_loop])
-	
+
 
 	def _init_device(self, *a, **k):
 		super(AumPush, self)._init_device(*a, **k)
 		self._device = self._device_component
 		debug('device_provider:', self._device_component._device_provider)
 		self._on_new_device_set.subject = self._device_provider
-	
+
 
 	@listens('device')
 	def _on_new_device_set(self):
 		self.schedule_message(1, self._select_note_mode)
-	
+
 
 	def _select_note_mode(self, mod_device = None):
 		if not self._main_modes.selected_mode is 'troll':
@@ -199,7 +201,7 @@ class AumPush(Push):
 					self._note_modes.selected_mode = 'instrument'
 				self.reset_controlled_track()
 				debug('new note mode is:', self._note_modes.selected_mode)
-	
+
 
 	def _on_selected_track_changed(self):
 		super(AumPush, self)._on_selected_track_changed()
@@ -207,22 +209,22 @@ class AumPush(Push):
 		self._main_modes.pop_groups(['add_effect'])
 		self._note_repeat_enabler.selected_mode = 'disabled'
 		self.schedule_message(1, self._select_note_mode)
-	
+
 
 	def disconnect(self):
-		self.log_message('<<<<<<<<<<<<<<<<<<<<<<<< AumPush ' + str(self._monomod_version) + ' log closed >>>>>>>>>>>>>>>>>>>>>>>>') 
+		self.log_message('<<<<<<<<<<<<<<<<<<<<<<<< AumPush ' + str(self._monomod_version) + ' log closed >>>>>>>>>>>>>>>>>>>>>>>>')
 		super(AumPush, self).disconnect()
-	
+
 
 	def set_highlighting_session_component(self, session_component):
 		self._highlighting_session_component = session_component
 		session_component and self._highlighting_session_component.set_highlighting_callback(self._set_session_highlight)
-	
+
 
 	def _can_auto_arm_track(self, track):
 		routing = track.current_input_routing
 		return routing == 'Ext: All Ins' or routing == 'All Ins' or routing.startswith('AumPush')
-	
+
 
 	def test_browser(self):
 		debug('test_browser')
@@ -239,8 +241,8 @@ class AumPush(Push):
 					if inneritem.name == 'Default.aupreset':
 						browser.load_item(inneritem)
 						break
-				
-	
+
+
 
 	def load_preset(self, target = None, folder = None, directory = 'defaultPresets'):
 		debug('load_preset()', target, folder, directory)
@@ -312,7 +314,7 @@ class AumPush(Push):
 										else:
 											debug(inneritem, 'item isnt loadable 4')
 											break
-	
+
 
 
 class ModDispayComponent(ControlSurfaceComponent):
@@ -327,33 +329,33 @@ class ModDispayComponent(ControlSurfaceComponent):
 		self._value_display_line = None
 		self._name_data_sources = [ DisplayDataSource(string) for string in display_strings ]
 		self._value_data_sources = [ DisplayDataSource(string) for string in value_strings ]
-	
+
 
 	def set_name_display_line(self, display_line):
 		self._name_display_line = display_line
 		if self._name_display_line:
 			self._name_display_line.set_data_sources(self._name_data_sources)
-	
+
 
 	def set_value_display_line(self, display_line):
 		self._value_display_line = display_line
 		if self._value_display_line:
 			self._value_display_line.set_data_sources(self._value_data_sources)
-	
+
 
 	def set_name_string(self, value, source = 0):
 		if source in range(len(self._name_data_sources)):
 			self._name_data_sources[source].set_display_string(str(value))
-	
+
 
 	def set_value_string(self, value, source = 0):
 		if source in range(len(self._value_data_sources)):
 			self._value_data_sources[source].set_display_string(str(value))
-	
+
 
 	def update(self):
 		pass
-	
+
 
 
 class ModShiftBehaviour(ModeButtonBehaviour):
@@ -361,17 +363,17 @@ class ModShiftBehaviour(ModeButtonBehaviour):
 
 	def press_immediate(self, component, mode):
 		component.push_mode(mode)
-	
+
 
 	def release_immediate(self, component, mode):
 		if len(component.active_modes) > 1:
 			component.pop_mode(mode)
-	
+
 
 	def release_delayed(self, component, mode):
 		if len(component.active_modes) > 1:
 			component.pop_mode(mode)
-	
+
 
 
 class PushModHandler(ModHandler):
@@ -397,20 +399,20 @@ class PushModHandler(ModHandler):
 		self._shifted = False
 		self._shift_display = ModDispayComponent(self, [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
 		self._alt_display = ModDispayComponent(self, [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
-	
+
 
 	def select_mod(self, mod):
 		super(PushModHandler, self).select_mod(mod)
 		self._script._select_note_mode()
 		self.update()
 		debug('modhandler select mod: ' + str(mod))
-	
+
 
 	def load_preset(self, value, *a, **k):
 		debug('recieve load preset value:', value)
 		if value > -1:
 			self._script.load_preset(value)
-	
+
 
 	def _receive_grid(self, x, y, value = -1, identifier = -1, channel = -1, *a, **k):
 		#debug('_receive_base_grid:', x, y, value, identifier, channel)
@@ -427,108 +429,108 @@ class PushModHandler(ModHandler):
 					new_channel = channel if channel > -1 else button._original_channel
 					button._msg_identifier != new_identifier and button.set_identifier(new_identifier)
 					button._msg_channel != new_channel and button.set_channel(new_channel)
-					button.set_enabled((channel, identifier) == (-1, -1))
-	
+					button.suppress_script_forwarding = not ((channel, identifier) == (-1, -1)) #button.set_enabled((channel, identifier) == (-1, -1))
+
 
 	def _receive_key(self, x, value):
 		#debug('_receive_key:', x, value)
 		if not self._keys_value.subject is None:
 			self._keys_value.subject.send_value(x, 0, self._push_colors[self._colors[value]], True)
-	
+
 
 	def _receive_key2(self, x, value):
 		#debug('_receive_key2:', x, value)
 		if not self._keys2_value.subject is None:
 			self._keys2_value.subject.send_value(x, 0, self._push_colors[self._colors[value]], True)
-	
+
 
 	def set_key2_buttons(self, keys):
 		self._keys2_value.subject = keys
 		if self.active_mod():
 			self.active_mod()._addresses['key2'].restore()
-	
+
 
 	@listens('value')
 	def _keys2_value(self, value, x, y, *a, **k):
 		debug('_keys2_value', value, x, y)
 		if self._active_mod:
 			self._active_mod.send('key2', x, value)
-	
+
 
 	def _receive_push_name_display(self, x, value):
 		if not self._shift_display is None:
 			self._shift_display.set_name_string(str(value), x)
-	
+
 
 	def _receive_push_value_display(self, x, value):
 		if not self._shift_display is None:
 			self._shift_display.set_value_string(str(value), x)
-	
+
 
 	def _receive_push_alt_name_display(self, x, value):
 		if not self._alt_display is None:
 			self._alt_display.set_name_string(str(value), x)
-	
+
 
 	def _receive_push_alt_value_display(self, x, value):
 		if not self._alt_display is None:
 			self._alt_display.set_value_string(str(value), x)
-	
+
 
 	def set_name_display_line(self, display):
 		if self._shift_display:
 			self._shift_display.set_name_display_line(display)
-	
+
 
 	def set_value_display_line(self, display):
 		if self._shift_display:
 			self._shift_display.set_value_display_line(display)
-	
+
 
 	def set_alt_name_display_line(self, display):
 		if self._alt_display:
 			self._alt_display.set_name_display_line(display)
 			debug('setting alt display')
-	
+
 
 	def set_alt_value_display_line(self, display):
 		if self._alt_display:
 			self._alt_display.set_value_display_line(display)
-	
+
 
 	def set_detent_dial(self, dial):
 		self._detent_dial_value.subject = dial
 		#if self.active_mod():
 		#	self.active_mod.send('detent_dial', value)
-	
+
 
 	def set_master_dial(self, dial):
 		self._master_dial_value.subject = dial
-	
+
 
 	def _receive_detent_dial(self, value):
 		if not self._detent_dial_value.subject is None:
 			self._detent_dial_value.subject.send_value(value)
-	
+
 
 	@listens('value')
 	def _detent_dial_value(self, value, *a, **k):
 		debug('_detent_dial_value', value)
 		if self._active_mod:
 			self._active_mod.send('detent_dial', value)
-	
+
 
 	@listens('value')
 	def _master_dial_value(self, value, *a, **k):
 		pass
-	
+
 
 	def update_device(self):
 		if self.is_enabled() and not self._script._device_component is None:
 			debug('modhandler.update_device')
 			self._script._device_component.update()
 			self._script._device_component._update_parameters()
-	
+
 
 	@listens('value')
 	def _shift_value(self, value, *a, **k):
@@ -546,7 +548,7 @@ class PushModHandler(ModHandler):
 			self.legacy_shift_layer and self.legacy_shift_layer.leave_mode()
 			self.shift_layer and self.shift_layer.leave_mode()
 		self.update()
-	
+
 
 	@Shift_button.pressed
 	def Shift_button(self, button):
@@ -559,7 +561,7 @@ class PushModHandler(ModHandler):
 		if mod and mod.legacy:
 			self.legacy_shift_layer and self.legacy_shift_layer.enter_mode()
 		self.update()
-	
+
 
 	@Shift_button.released
 	def Shift_button(self, button):
@@ -570,7 +572,7 @@ class PushModHandler(ModHandler):
 		self.legacy_shift_layer and self.legacy_shift_layer.leave_mode()
 		self.shift_layer and self.shift_layer.leave_mode()
 		self.update()
-	
+
 
 	@Alt_button.pressed
 	def Alt_button(self, button):
@@ -583,7 +585,7 @@ class PushModHandler(ModHandler):
 			mod._device_proxy.update_parameters()
 		self.alt_layer and self.alt_layer.enter_mode()
 		self.update()
-	
+
 
 	@Alt_button.released
 	def Alt_button(self, button):
@@ -595,7 +597,7 @@ class PushModHandler(ModHandler):
 			mod._device_proxy.update_parameters()
 		self.alt_layer and self.alt_layer.leave_mode()
 		self.update()
-	
+
 
 	def update(self, *a, **k):
 		mod = self.active_mod()
@@ -606,11 +608,11 @@ class PushModHandler(ModHandler):
 				self._grid_value.subject.reset()
 			if not self._keys_value.subject is None:
 				self._keys_value.subject.reset()
-	
+
 
 	def reevaluate_device(self):
 		self._script._device_provider.reevaluate_device()
-	
+
 
 
 
