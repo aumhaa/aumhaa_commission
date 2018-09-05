e441188de4e14aad153d232a20f6e65ad3fc7646
aumhaa <aumhaa@gmail.com>
2018-09-05 16:57:48 -0700

    Removed OJI material.

--- Crate/__init__.py
+++
@@ -1,8 +0,0 @@
-# by amounra 0715 : http://www.aumhaa.com
-
-from Crate import Crate
-
-def create_instance(c_instance):
-    """ Creates and returns the Crate script """
-    return Crate(c_instance)
-
