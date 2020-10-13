45ef65b7824e82d6931a5336857b4dc58335d6b5
aumhaa <aumhaa@gmail.com>
2020-10-12 22:55:02 -0700

    onesevenfive   :   commissioned script based on MonoPedal that allows loopers to send state to rgb LEDs.

---
+++ onesevenfive/MattLooper/__init__.py
@@ -0,0 +1,14 @@
+# by amounra 1020 : http://www.aumhaa.com
+
+from __future__ import absolute_import, print_function
+from ableton.v2.control_surface.capabilities import *
+from .MattLooper import MattLooper
+
+def create_instance(c_instance):
+	""" Creates and returns the MonoPedal script """
+	return MattLooper(c_instance)
+
+
+def get_capabilities():
+	return {CONTROLLER_ID_KEY: controller_id(vendor_id=2536, product_ids=[115], model_name='aumhaa MattLooper'),
+	 PORTS_KEY: [inport(props=[NOTES_CC, SCRIPT, REMOTE]), outport(props=[SCRIPT, REMOTE])]}
